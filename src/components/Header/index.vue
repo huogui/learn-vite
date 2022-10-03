e1b4488a577c2daf3362e202ddc14fcca4fcf09f
huogui <645039234@qq.com>
2022-10-03 23:41:37 +0800

    chore: test wasm

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import logoSrc from '@assets/vue.svg'
+import init from '@wasm/fib.wasm?init'
 
+type FibFunc = (num: number) => number
+
+init({}).then(({ exports }) => {
+  const fibFunc = exports.fib as FibFunc
+  console.log('Fib result:', fibFunc(10))
+})
 </script>
 
 <template>
-  <p class="header-1">This is Header</p>
+  <p class="header-1">
+    This is Header
+  </p>
+  <img :src="logoSrc" alt="">
 </template>
 
 <style scoped lang="scss">
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,36 @@
 import path from 'path'
-import { defineConfig,normalizePath } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import autoprefixer from 'autoprefixer';
+import autoprefixer from 'autoprefixer'
+import SvgLoader from 'vite-svg-loader'
 
 // 用 normalizePath 解决 window 下的路径问题
-const variablePath = normalizePath(path.resolve('./src/variable.scss'));
-console.log(variablePath)
+const variablePath = normalizePath(path.resolve('./src/variable.scss'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // css 相关的配置
+  resolve: {
+    alias: {
+      '@assets': path.join(__dirname, 'src/assets'),
+      '@wasm': path.join(__dirname, 'src/wasm'),
+    },
+  },
+  // css 相关的配置
   css: {
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "./src/variable.scss";`
-      }
+        additionalData: `@import "${variablePath}";`,
+      },
     },
     postcss: {
       plugins: [
         autoprefixer({
           // 指定目标浏览器
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
-        })
-      ]
-    }
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ],
+    },
   },
-  plugins: [vue()]
+  plugins: [vue(), SvgLoader()],
 })
