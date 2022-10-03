bea15a8a4f07d0de4b7e8f821ce20db5b9f0d411
huogui <645039234@qq.com>
2022-10-03 22:50:23 +0800

    chore: add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-  import Header from './components/Header/index.vue'
+import Header from './components/Header/index.vue'
 </script>
 
 <template>
   <div>
-   <Header/>
+    <Header />
   </div>
 </template>
