b4993fdeb3ac6bead7f849454f221bb65eac7813
huogui <645039234@qq.com>
2022-10-03 16:45:44 +0800

    feat: vite初体验

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.join(__dirname, '.'),
   plugins: [vue()]
 })
